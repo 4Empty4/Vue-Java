4fa7fbeb684bc3fa702c41cede1ff973950e7002
4Empty4 <120721789+4Empty4@users.noreply.github.com>
2024-01-31 16:14:14 +0100

    Add defoult file

---
+++ src/main/java/com/example/vuejava/VueJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.vuejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VueJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vuejava/VueJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vuejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
