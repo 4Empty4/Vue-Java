e8687322745674523b8cde659a532e8d54c11923
nadiiapavlus <126113363+nadiiapavlus@users.noreply.github.com>
2024-01-31 17:00:25 +0100

    add App.vue

---
+++ src/main/resources/templates/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+    <button @click="changeMessage">Змінити повідомлення</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Привіт, це моє перше Vue.js застосування!'
+    };
+  },
+  methods: {
+    changeMessage() {
+      this.message = 'Повідомлення було змінено!';
+    }
+  }
+};
+</script>
+
+<style scoped>
+h1 {
+  color: blue;
+}
+</style>
